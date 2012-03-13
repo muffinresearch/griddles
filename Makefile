d2f1549c6752c7a416919e6da037d6aed4f4ce20
Stuart Colville <muffinresearchlabs@gmail.com>
2012-03-13 18:04:30 +0000

    utilise sass for generation

---
+++ Makefile
@@ -0,0 +1,19 @@
+COMPASS=/usr/bin/compass
+
+compass-watch:
+	$(COMPASS) watch -c config/compass/debug.rb
+
+release: compass-min compass-debug compass-expanded
+
+compass-min:
+	$(COMPASS) compile -c config/compass/min.rb
+
+compass-debug:
+	$(COMPASS) compile -c config/compass/debug.rb
+
+compass-expanded:
+	$(COMPASS) compile -c config/compass/expanded.rb
+
+.PHONY: compass-watch release compass-min compass-debug compass-expanded
+
+.NOTPARALLEL:
---
+++ config/compass/expanded.rb
@@ -0,0 +1,20 @@
+# Require any additional compass plugins here.
+root_path = File.expand_path(File.join(File.dirname(__FILE__), "../../"))
+project_path = root_path
+css_path = File.join(root_path, "css/expanded/")
+sass_path = File.join(root_path, "scss/")
+cache_path = File.join(root_path, "tmp/sass-cache")
+images_path = File.join(root_path, "img/")
+javascripts_path = File.join(root_path, "js/")
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+
+
